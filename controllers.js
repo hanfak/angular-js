d64a56e8f21205e597df911b9861974fcf349a7b
Gabrielle <gabivk@gmail.com>
2016-04-18 14:51:20 +0100

    added controller

---
+++ controllers.js
@@ -0,0 +1,8 @@
+toDoApp.controller('ToDoController', function() {
+  // var toDoList = this;
+  // toDoList.toDos = [
+  //   {text: 'ToDo1', done: false},
+  //
+  // ];
+  this.toDo = 'ToDo1';
+});
