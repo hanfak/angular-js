5be2c85457b483929dd8d20ad369768d89d91221
Hanif Fakira <fakira.work@gmail.com>
2016-04-18 15:46:16 +0100

    Q4 Completed, no check box

--- controllers.js
+++ controllers.js
@@ -2,7 +2,7 @@ toDoApp.controller('ToDoController', function() {
 
     this.toDos = [
     {text: 'ToDo1', completed: false},
-    {text: 'ToDo2', completed: false},
+    {text: 'ToDo2', completed: true},
     {text: 'ToDo3', completed: false},
   ];
   // this.toDo = ['ToDo1', 'ToDo2', 'Todo3'];
