467dc98544cc95bb1f7f5634a652c81cdb49bd63
Hanif Fakira <fakira.work@gmail.com>
2016-04-19 14:00:21 +0100

    Q7 Refactored

--- ToDoService.js
+++ ToDoService.js
@@ -1,13 +1,22 @@
 toDoApp.service('ToDoService', ['$http', 'ToDoFactory', function($http, ToDoFactory) {
-  this.getAll = function() {
-    var todos = [];
-    $http.get('http://quiet-beach-24792.herokuapp.com/todos.json')
-      .then(function(response) {
-        response.data.forEach(function(data) {
-          todos.push(new ToDoFactory(data.text));
-        });
-      }, function(err) {});
+  var self = this;
 
+  self.getAll = function() {
+    var todos = [];
+    _fetchTodosFromApi(todos);
     return todos;
   };
+
+  function _fetchTodosFromApi(todos) {
+    $http.get('http://quiet-beach-24792.herokuapp.com/todos.json')
+      .then(function(resp) {
+        _handleResponseFromApi(resp.data, todos);
+      }, function(err) {});
+  }
+
+  function _handleResponseFromApi (data, todos) {
+    data.forEach(function(toDoData){
+      todos.push(new ToDoFactory(toDoData.text));
+    });
+  }
 }]);
